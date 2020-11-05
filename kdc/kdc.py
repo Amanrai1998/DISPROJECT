9cf2eb59a760f670c6baf603d8cfcc4242a86c67
unknown <aman4pegasus@gmail.com>
2020-11-05 11:16:22 +0530

    add KDC registration function

--- kdc/kdc.py
+++ kdc/kdc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import socket
+import secrets
 
 #GLOBALS
 class User:
@@ -26,23 +27,15 @@ class DB:   #DATABASE
                 return user
         return None
 
-db = DB()
+    def registerUser(self, addr, nType):
+        #generate a Unique Symmentric Key And ID
+        ID = secrets.token_hex(14)  #Check Uniqueness
+        SK = secrets.token_hex(32)
+        #add db entry ID, IP, PORT, Nodetype, symmetric key, FILES, Session Key with expiry(empty)
+        user = User(ID,addr.UDP_IP,addr.UDP_PORT,nType,SK,[],None)
+        self.User.append(user)
 
-async def registration(s, addr, data):
-    #check in db if server with ip exist
-    user = db.findUserByAddr(addr)
-
-    if(user != None):
-        print("Node Already Registered")
-        return
-
-    #if fsi or dsi
-
-    #generate a Unique Symmentric Key And ID
-    #add db entry ID, IP, PORT, Nodetype, symmetric key, FILES, Session Key with expiry(empty)
-
-    #Send Registration Confirmation
-    print("Registration Function")
+        return user
 
 UDP_IP = '127.0.0.1'
 UDP_PORT = 2000
@@ -51,10 +44,28 @@ BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((UDP_IP, UDP_PORT))
 
+db = DB()
+
+async def registration(addr, data):
+    #check in db if server with ip exist
+    user = db.findUserByAddr(addr)
+
+    if(user != None):
+        message = "AlreadyRegistered"
+        s.sendto(message, (addr.UDP_IP, addr.UDP_PORT))
+        return
+
+    #register user
+    user = db.registerUser(addr, data[1])
+
+    #Send Registration Confirmation
+    message = "Registered "+user.ID+" "+user.SK
+    s.sendto(message, (addr.UDP_IP, addr.UDP_PORT))
+
 data,addr = s.recvfrom(BUFFER_SIZE)
 
 data = data.split(' ')
 
 if(data[0]=="Register"):
     #Registration Module
-    registration(s, addr, data)
+    registration(addr, data)
